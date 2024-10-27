2268e1d5fbb9364816b4227d9820dbd572376752
NOTxV0Ltz <il_firsov@mail.ru>
2024-10-27 23:10:06 +0900

    Добавление классов для животных

---
+++ animals.py
@@ -0,0 +1,11 @@
+class Животное:
+    def init(self, имя):
+        self.имя = имя
+
+class ДомашниеЖивотные(Животное):
+    def __init(self, имя):
+        super().init(имя)
+
+class ВьючныеЖивотные(Животное):
+    def init(self, имя):
+        super().init(имя)
